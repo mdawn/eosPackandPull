63687c56d0ed6f463d99eb2bc353280830011082
Misty Dawn Hall <mistydawncarmichael@gmail.com>
2018-10-06 21:39:50 -0400

    ensuring connection to local node

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+var request = require("request");
+
+// verifying connection to EOS node by logging a single block to the console
+var options = {
+  method: 'POST',
+  url: 'http://127.0.0.1:8888/v1/chain/get_block',
+  body: '{"block_num_or_id":"7777"}'
+};
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+
+var bodyParser = require('body-parser');
+var http = require('http');
+
 app.get('/', (req, res) => {
   res.send('Howdy');
 });
+
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
