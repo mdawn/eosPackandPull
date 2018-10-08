bc98555ba69c95743728d6a53838d428ad12d134
Misty Dawn Hall <mistydawncarmichael@gmail.com>
2018-10-07 21:41:21 -0400

    connected to mongoDB

--- app.js
+++ app.js
@@ -5,14 +5,27 @@ const path = require('path');
 app.use(express.static(path.resolve(__dirname, 'public')));
 const eos = EosApi();
 
+
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/eostest", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+});
+
 var request = require("request");
 var blockId = 12;
+
+
 
 // verifying connection to EOS node by logging a single block to the console
 var options = {
   method: 'POST',
   url: 'http://127.0.0.1:8888/v1/chain/get_block',
-  body: '{"block_num_or_id":"12"}'
+  body: '{"block_num_or_id":"6945"}'
 };
 
 eos.getBlock({block_num_or_id: blockId})
@@ -22,7 +35,6 @@ eos.getInfo({})
   .then(result => console.log(result));
 
   // write to mongodb
-
 
 
 var bodyParser = require('body-parser');
