824b06deb03afca114040cbac4ace4dd3f6cceea
Misty Dawn Hall <mistydawncarmichael@gmail.com>
2018-10-09 22:47:18 -0400

    pull recent blocks in front end

--- public/eos-fizzbuzz-frontend.js
+++ public/eos-fizzbuzz-frontend.js
@@ -1,1 +1,14 @@
-console.log('foo');
+function updateMostRecentBlock(newText) {
+  console.log(`updating block with ${newText}`);
+  document.getElementById('recent-block').innerHTML = newText;
+}
+
+function getRecentBlockAndUpdatePage() {
+  fetch('http://localhost:3000/show').then(function(response) {
+    return response.json();
+  }).then(function(myJson) {
+    updateMostRecentBlock(JSON.stringify(myJson));
+  });
+}
+
+document.getElementById('latest-block-button').onclick = getRecentBlockAndUpdatePage;
