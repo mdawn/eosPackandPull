d2832da7e8d2fd6cd4cdd8e7cca5703dfddfeba6
Misty Dawn Hall <mistydawncarmichael@gmail.com>
2018-10-10 18:08:14 -0400

    graphQL endpoint connected to eosjs

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const EosApi = require('eosjs-api');
 const express = require('express');
 const app = express();
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+const fs = require('fs');
 const path = require('path');
 app.use(express.static(path.resolve(__dirname, 'public')));
 const eos = EosApi();
@@ -44,6 +47,82 @@ app.get('/show', (req, res) => {
   eos.getInfo({})
     .then(result => res.send(result));
 });
+
+async function fetchBlock(blockNum){
+        try{
+          const block = await eos.getBlock(blockNum);
+          return block;
+        }
+        catch(err){
+          console.log("EOS API returned an error; check block number")
+          return {error: String(err)};
+      }
+  }
+
+
+//function for fetching block data from db or EOS node
+async function getBlockData(blockNum){
+  blockNum = blockNum || -1;
+  if (blockNum === -1){
+    const data = await eos.getInfo({});
+    const lastBlockNum = data['head_block_num'];
+    return await fetchBlock(lastBlockNum);
+  }
+  else {
+    return await fetchBlock(blockNum);
+  }
+}
+
+/*define graphQL query schema;
+some numbers like block_num and ref_block_prefix are better as String
+since they are too large to be represented as a 32-bit Int type by GraphQL */
+const schemaFile = fs.readFileSync(__dirname + '/schema.graphqls', 'utf8')
+const schema = buildSchema(schemaFile)
+
+
+//for resolving queries to api
+let resolveBlocks = async function(args) {
+  if (args.numbers) {
+      const numbers = args.numbers;
+      const blocks = [];
+      for (const blockNum of numbers){
+        // console.log(blockNum);
+        blocks.push(await getBlockData(blockNum));
+      }
+      return await blocks;
+  }
+  else {
+      return [await getBlockData()];
+  }
+}
+
+let resolveLastBlock = async function(args) {
+    return await getBlockData();
+}
+
+//define root query
+let rootQuery = {
+  blocks: resolveBlocks,
+  lastBlock: resolveLastBlock
+};
+
+//graphQL endpoint
+var graphQLapp = express();
+graphQLapp.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: rootQuery,
+  graphiql: true,
+}));
+
+graphQLapp.listen(4000);
+console.log('GraphQL server at localhost:4000/graphql');
+
+//api-route for eos block info
+app.use('/blocks', graphqlHTTP({
+  schema: schema,
+  rootValue: rootQuery,
+  graphiql: true,
+}));
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
