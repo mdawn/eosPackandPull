70229708350b53fecdcf26af7e93da32645d2470
Misty Dawn Hall <mistydawncarmichael@gmail.com>
2018-10-11 22:39:16 -0400

    pull selected block on front end

--- app.js
+++ app.js
@@ -1,59 +1,17 @@
 const EosApi = require('eosjs-api');
 const express = require('express');
-const app = express();
+//const app = express();
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
 const fs = require('fs');
 const path = require('path');
-app.use(express.static(path.resolve(__dirname, 'public')));
+//app.use(express.static(path.resolve(__dirname, 'public')));
 const eos = EosApi();
 const Block = require('./block');
 const mongoose = require('mongoose');
 
 
 mongoose.connect('mongodb://localhost:27017/eostest', {poolSize: 1});
-
-
-var blockId = 12;
-
-// Retrieve
-var MongoClient = require('mongodb').MongoClient;
-/*
-// log block in the db
-MongoClient.connect("mongodb://localhost:27017/eostest", function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("eostest");
-  eos.getBlock({block_num_or_id: blockId})
-  .then(result => {
-    dbo.collection("eosBlocks").insertOne(result, function(err, res) {
-      if (err) throw err;
-      console.log("1 block inserted");
-      db.close();
-    });
-  });
-});
-*/
-
-// verifying connection to EOS node by logging a single block to the console
-var options = {
-  method: 'POST',
-  url: 'http://127.0.0.1:8888/v1/chain/get_block',
-  body: '{"block_num_or_id":"6945"}'
-};
-
-eos.getBlock({block_num_or_id: blockId})
-.then(result => console.log(result));
-
-eos.getInfo({})
-.then(result => console.log(result));
-
-var bodyParser = require('body-parser');
-var http = require('http');
-
-app.get('/show', (req, res) => {
-  eos.getInfo({})
-  .then(result => res.send(result));
-});
 
 async function fetchBlock(blockNum){
   console.log("whiskey");
@@ -140,16 +98,7 @@ graphQLapp.use('/graphql', graphqlHTTP({
   graphiql: true,
 }));
 
+graphQLapp.use(express.static(path.resolve(__dirname, 'public')));
+
 graphQLapp.listen(4000);
 console.log('GraphQL server at localhost:4000/graphql');
-
-//api-route for eos block info
-app.use('/blocks', graphqlHTTP({
-  schema: schema,
-  rootValue: rootQuery,
-  graphiql: true,
-}));
-
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
-});
--- public/eos-fizzbuzz-frontend.js
+++ public/eos-fizzbuzz-frontend.js
@@ -3,12 +3,73 @@ function updateMostRecentBlock(newText) {
   document.getElementById('recent-block').innerHTML = newText;
 }
 
+function updatePulledBlock(newText) {
+  document.getElementById('pulled-block').innerHTML = newText;
+}
+
 function getRecentBlockAndUpdatePage() {
-  fetch('http://localhost:3000/show').then(function(response) {
-    return response.json();
-  }).then(function(myJson) {
-    updateMostRecentBlock(JSON.stringify(myJson));
+  fetch('http://localhost:4000/graphql?', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: `{
+      lastBlock {
+        id
+        timestamp
+        txn_count
+        block_num
+      }
+    }`,
+      variables: true,
+      operationName: true
+    }),
+    credentials: 'include'
+  }).then(function(response) {
+    return response.text();
+  }).then(function(responseBody) {
+    try {
+      updateMostRecentBlock(responseBody);
+    } catch (error) {
+      return responseBody;
+    }
+  });
+}
+
+function getPulledBlockAndUpdatePage() {
+  var blockID = document.querySelector('input').value;
+  fetch('http://localhost:4000/graphql?', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: `{
+   blocks(numbers: [${blockID}]){
+     id
+     timestamp
+     txn_count
+     block_num
+   }
+ }
+`,
+      variables: true,
+      operationName: true
+    }),
+    credentials: 'include'
+  }).then(function(response) {
+    return response.text();
+  }).then(function(responseBody) {
+    try {
+      updatePulledBlock(responseBody);
+    } catch (error) {
+      return responseBody;
+    }
   });
 }
 
 document.getElementById('latest-block-button').onclick = getRecentBlockAndUpdatePage;
+document.getElementById('pulled-block-button').onclick = getPulledBlockAndUpdatePage;
