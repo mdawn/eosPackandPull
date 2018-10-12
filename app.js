c52ae49e2d047971b7656311c4fa129b07982616
Misty Dawn Hall <mistydawncarmichael@gmail.com>
2018-10-11 21:40:39 -0400

    store retrieved blocks in mongodb

--- app.js
+++ app.js
@@ -7,25 +7,32 @@ const fs = require('fs');
 const path = require('path');
 app.use(express.static(path.resolve(__dirname, 'public')));
 const eos = EosApi();
+const Block = require('./block');
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost:27017/eostest', {poolSize: 1});
+
 
 var blockId = 12;
 
 // Retrieve
 var MongoClient = require('mongodb').MongoClient;
-
+/*
 // log block in the db
 MongoClient.connect("mongodb://localhost:27017/eostest", function(err, db) {
   if (err) throw err;
   var dbo = db.db("eostest");
   eos.getBlock({block_num_or_id: blockId})
-    .then(result => {
-      dbo.collection("eosBlocks").insertOne(result, function(err, res) {
-        if (err) throw err;
-        console.log("1 block inserted");
-        db.close();
-      });
+  .then(result => {
+    dbo.collection("eosBlocks").insertOne(result, function(err, res) {
+      if (err) throw err;
+      console.log("1 block inserted");
+      db.close();
     });
+  });
 });
+*/
 
 // verifying connection to EOS node by logging a single block to the console
 var options = {
@@ -35,33 +42,50 @@ var options = {
 };
 
 eos.getBlock({block_num_or_id: blockId})
-  .then(result => console.log(result));
+.then(result => console.log(result));
 
 eos.getInfo({})
-  .then(result => console.log(result));
+.then(result => console.log(result));
 
 var bodyParser = require('body-parser');
 var http = require('http');
 
 app.get('/show', (req, res) => {
   eos.getInfo({})
-    .then(result => res.send(result));
+  .then(result => res.send(result));
 });
 
 async function fetchBlock(blockNum){
-        try{
-          const block = await eos.getBlock(blockNum);
-          return block;
-        }
-        catch(err){
-          console.log("EOS API returned an error; check block number")
-          return {error: String(err)};
-      }
+  console.log("whiskey");
+  const dbBlock = await Block.getBlockByNum(blockNum);
+  console.log("tango");
+  if(dbBlock){
+    return dbBlock;
   }
+  else {
+    try{
+      const block = await eos.getBlock(blockNum);
+      try{
+        const addedBlock = await Block.addBlock(block);
+        // console.log(await addedBlock);
+        return block;
+      }
+      catch(err){
+        //error adding block to cache, just return block
+        return block;
+      }
+    }
+    catch(err){
+      console.log("EOS API returned an error; check block number")
+      return {error: String(err)};
+    }
+  }
+}
 
 
 //function for fetching block data from db or EOS node
 async function getBlockData(blockNum){
+  console.log("getBlockData");
   blockNum = blockNum || -1;
   if (blockNum === -1){
     const data = await eos.getInfo({});
@@ -82,22 +106,24 @@ const schema = buildSchema(schemaFile)
 
 //for resolving queries to api
 let resolveBlocks = async function(args) {
+  console.log("resolveBlocks");
+  console.log(args);
   if (args.numbers) {
-      const numbers = args.numbers;
-      const blocks = [];
-      for (const blockNum of numbers){
-        // console.log(blockNum);
-        blocks.push(await getBlockData(blockNum));
-      }
-      return await blocks;
+    const numbers = args.numbers;
+    const blocks = [];
+    for (const blockNum of numbers){
+      // console.log(blockNum);
+      blocks.push(await getBlockData(blockNum));
+    }
+    return await blocks;
   }
   else {
-      return [await getBlockData()];
+    return [await getBlockData()];
   }
 }
 
 let resolveLastBlock = async function(args) {
-    return await getBlockData();
+  return await getBlockData();
 }
 
 //define root query
@@ -125,5 +151,5 @@ app.use('/blocks', graphqlHTTP({
 }));
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
